aecc367cacdff0e08d26146c7f4fa0cf1b3a0ef3
natashang <50751197+natashang@users.noreply.github.com>
2020-02-18 13:17:59 -0800

    req.body.emailfield works! pulling from html input

--- daySixteen/expressCommentForm/index.js
+++ daySixteen/expressCommentForm/index.js
@@ -1,53 +1,43 @@
+// import block -- for all of the dependencies and module
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+
+
+//const html  = require('./appView/formview.js')
+
+
 const app = express()
 const port = 3000
 
-var fs = require('fs')
+//app.use(express.urlencoded())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-const { html } = require('./appView/view')
-
-
-app.set("view engine", "pug")
-app.set("views", path.join(__dirname, "appView"))
+/*
+app.set("view engine", "html")
+app.set("views", path.join(__dirname + "appView"))
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-
-  /*  
-fs.readFile('./filesystemtext.txt', (err,data) => {
-    process.stdout.write(data)
-})
 */
-    fs.readFile('./form.html', (error, data) => {
-        /*
-        if (error) {
-            console.log("houston we got a problem")
-            res.writeHead(500)
-            res.end()
-        }
-        else {
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(content, 'utf-8')
-        }
-        */
-       res.writeHead(200, {'Content-Type': 'text/html'})
-    })
+
+app.get('/', (req, res) => { 
+    console.log("dirname: ", __dirname)
+    //    res.sendFile(path.join(__dirname+'/appView/formview.js'));
+    res.sendFile(path.join(__dirname+'/appView/form.html'));
+
 })
 
 app.get('/history', (req, res) => {
-    //res.render('history');
     console.log("at history")
 })
 
 app.post('/history', (req, res) => {
     console.log('oi')
-//    console.log(req.body.commentfield)
-    //var comment = req.body.commentfield
-   //console.log("the comment is: %s", comment)
+    console.log(req.body.emailfield)
+    console.log(req.body.commentfield)
 })
-
-//app.post('/histor')
 
 app.listen(
     port,
