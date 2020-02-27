b74c9281d0791ed3603975dc788280f33a713768
natashang <50751197+natashang@users.noreply.github.com>
2020-02-27 08:49:21 -0800

    Updates

---
+++ precache-manifest.a7499f6aa7ff168dddc92b478cd9fabf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadf0f26669ce0b5c8b619551f4051d3",
+    "url": "/basic-to-do-app/index.html"
+  },
+  {
+    "revision": "e8cee3b9481326152698",
+    "url": "/basic-to-do-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3725138f5761bc8403e0",
+    "url": "/basic-to-do-app/static/css/main.f1eec35d.chunk.css"
+  },
+  {
+    "revision": "e8cee3b9481326152698",
+    "url": "/basic-to-do-app/static/js/2.1c1cf65a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/basic-to-do-app/static/js/2.1c1cf65a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3725138f5761bc8403e0",
+    "url": "/basic-to-do-app/static/js/main.1790d110.chunk.js"
+  },
+  {
+    "revision": "bf3ba1a5e9c27a878bd8",
+    "url": "/basic-to-do-app/static/js/runtime-main.8f6b67f2.js"
+  }
+]);
