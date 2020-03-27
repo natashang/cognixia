4109067449dc4d6c38413afdd06a521c3f8ca143
natashang <50751197+natashang@users.noreply.github.com>
2020-03-26 19:18:14 -0700

    applied grid layout css for users

--- React/paginationApp/my-app/src/components/container/container.js
+++ React/paginationApp/my-app/src/components/container/container.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import User from '../user/user'
-
-// TODO: Make the users display in a row by col layout, than one list!
+import React from "react";
+import User from "../user/user";
+import "./container.css";
 
 // contains all of the users belonging to the page from the JSON file
-export default function Container(props){
-    return(
-        <div >
-            { props.listOfUsers.map(user => 
-                <User
-                    email={user.email}
-                    first_name={user.first_name}
-                    last_name={user.last_name}
-                    image={user.avatar}
-                />
-            )}
-        </div>
-    )
+export default function Container(props) {
+  return (
+    <div className="container">
+      {props.listOfUsers.map(user => (
+        <User
+          email={user.email}
+          first_name={user.first_name}
+          last_name={user.last_name}
+          image={user.avatar}
+        />
+      ))}
+    </div>
+  );
 }
--- React/paginationApp/my-app/src/components/router/router.js
+++ React/paginationApp/my-app/src/components/router/router.js
@@ -1,33 +1,35 @@
-import React from 'react'
-import { BrowserRouter, Link } from 'react-router-dom'
-import {Button, ButtonToolbar} from 'react-bootstrap'
+import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Button, ButtonToolbar } from "react-bootstrap";
+import "./router.css";
 
-export default function Router(props){
-    const style={
-        'margin':'15px',
-        'padding':'10px'
-    }
+export default function Router(props) {
+  const style = {
+    margin: "15px",
+    padding: "10px"
+  };
 
-    return(
-        <div>
-            <ButtonToolbar>
-                <BrowserRouter>
-                    {props.links.map ( link => {
-                        return(
-                            <Button 
-                                onClick={() => props.changePage(link.to) }
-                                variant="outline-primary"
-                                style={style}
-                            >
-                                <Link to={ link.to}>{link.name}</Link>
-                            </Button>
-                        )
-                    })}
-                </BrowserRouter>    
-            </ButtonToolbar>
-        </div>
-        
-
-        
-    )
+  return (
+    <div>
+      <ButtonToolbar>
+        <BrowserRouter>
+          <div className="containerRouter">
+            {props.links.map(link => {
+              return (
+                <>
+                  <Button
+                    onClick={() => props.changePage(link.to)}
+                    variant="outline-primary"
+                    style={style}
+                  >
+                    <Link to={link.to}>{link.name}</Link>
+                  </Button>
+                </>
+              );
+            })}
+          </div>
+        </BrowserRouter>
+      </ButtonToolbar>
+    </div>
+  );
 }
